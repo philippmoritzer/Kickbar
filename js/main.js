0f5344b9dee1db8e8cdce0d14d29beedfe0d9b4c
Philipp Moritzer <philipp.moritzer@yahoo.de>
2019-08-13 19:34:07 +0200

    work in progress

--- js/main.js
+++ js/main.js
@@ -3,10 +3,23 @@
 //activeState[2] = learn
 const activeState = [false, false, false];
 
+var $impress = $("#impress");
+
 $(document).ready(function() {
   $("#impress-menu").on("click", () => {
-    $.get("html/impress.html", function(data) {
-      $("#impress").append(data);
-    });
+    if (activeState[0] === false) {
+      $.get("html/impress.html", function(data) {
+        $impress.append(data);
+        $impress.show("slow");
+        activeState[0] = true;
+      });
+    }
   });
 });
+
+function setActiveState(section) {
+  for (let aState of activeState) {
+    aState = false;
+  }
+  section = true;
+}
