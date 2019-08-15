22199d259f98a7fe30ce4a9eb5168ea109bf4389
Philipp Moritzer <philipp.moritzer@yahoo.de>
2019-08-15 20:06:00 +0200

    durchstich navigation/animation, work in progress: layout der kategorien

--- js/main.js
+++ js/main.js
@@ -3,23 +3,60 @@
 //activeState[2] = learn
 const activeState = [false, false, false];
 
-var $impress = $("#impress");
+var $impress_section = $("#impress");
+var $practice_section = $("#practice");
+var $learn_section = $("#learn");
+
+loadContent();
+
+function loadContent() {
+  $(document).ready(function() {
+    $.get("html/impress.html", function(data) {
+      $impress_section.append(data);
+    });
+    $.get("html/practice.html", function(data) {
+      $practice_section.append(data);
+    });
+    $.get("html/learn.html", function(data) {
+      $learn_section.append(data);
+    });
+  });
+}
 
 $(document).ready(function() {
-  $("#impress-menu").on("click", () => {
-    if (activeState[0] === false) {
-      $.get("html/impress.html", function(data) {
-        $impress.append(data);
-        $impress.show("slow");
-        activeState[0] = true;
-      });
-    }
+  $impress_section.on("click", () => {
+    activateSection($("#impress-content-container"), 0);
+  });
+  $practice_section.on("click", () => {
+    activateSection($("#practice-content-container"), 0);
+  });
+  $learn_section.on("click", () => {
+    activateSection($("#learn-content-container"), 0);
+  });
+  $("#logo-image").on("click", () => {
+    removeAllActiveSections();
   });
 });
+
+function activateSection(element, index) {
+  if (activeState[index]) {
+    removeAllActiveSections();
+    activeState[index] = false;
+  } else {
+    element.addClass("active");
+    setActiveState(index);
+  }
+}
+
+function removeAllActiveSections() {
+  $("#impress-content-container").removeClass("active");
+  $("#practice-content-container").removeClass("active");
+  $("#learn-content-container").removeClass("active");
+}
 
 function setActiveState(section) {
   for (let aState of activeState) {
     aState = false;
   }
-  section = true;
+  activeState[section] = true;
 }
